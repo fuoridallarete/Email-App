330dd131c6b6da6e43a46230d6e736f6a9fcb170
sa cux <fuoridallarete@users.noreply.github.com>
2019-03-28 17:23:32 +0100

    Last Commit
    create method to display all the requested info, cleanup of the code.

--- Email.java
+++ Email.java
@@ -19,18 +19,15 @@ public class Email {
     //System.out.println("Email created: " + this.firstName + " " + this.lastName);
     //call a method asking for the department which is gonna return the department
     this.department = setDepartment();
-    System.out.println("Your department is " + department);
     //call a method that returns a random password
     this.password = randomPassword(defaultPasswordLength);
     System.out.println("Your password is " + password);
     //combine elements to generate email
     email = firstName.toLowerCase() + lastName.toLowerCase() + "@" + department.toLowerCase() + "." + companySuffix;
-    System.out.println("Your newly generated email is: " + email);
   }
   //ask for the department
   private String setDepartment(){
-    System.out.println("DEPARTMENT CODES:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none ");
-    System.out.println("Enter your department code: ");
+    System.out.println("New Worker: " + firstName + ".\nEnter your DEPARTMENT CODE, " + firstName + ":\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none ");
     Scanner in = new Scanner(System.in);
     int depChoice = in.nextInt();
     if(depChoice == 1) return "Sales";
@@ -68,5 +65,12 @@ public class Email {
   }
   public String getPassword(){
     return password;
+  }
+  //method to display all the info
+  public String showInfo(){
+    return "DISPLAY NAME: " + firstName + " " +  lastName +
+           ",\nEMAIL COMPANY: " + email +
+        "\nMAILBOX CAPACITY: " + mailBoxCapacity + " mb";
+
   }
 }
